d64e05f6ce39237378c16b43970e98b5fd855f99
madhavsharma2106 <madhavsharma2106@gmail.com>
2021-05-11 19:21:11 +0530

    disabled exhaustive deps

--- src/components/memory-game/Component.jsx
+++ src/components/memory-game/Component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { createDeck } from "../../utils/misc";
 import { Card } from "../card/Card";
